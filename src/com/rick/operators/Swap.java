108445f61b8c96b5f9e2225e003c73ff4b0f9659
Rick <rick8220602@gmail.com>
2020-07-22 12:42:37 +0800

    「Swap」的幾種方式

---
+++ src/com/rick/operators/Swap.java
@@ -0,0 +1,36 @@
+package com.rick.operators;
+
+public class Swap {
+
+    public static void main(String[] args) {
+        int a = 1, b = 2;
+
+        // by 「額外空間法」
+        int temp = a;
+        a = b;
+        b = temp;
+
+        // by 「四則運算法」之「加減法」
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        // 一行寫法
+        a = a + b - (b = a);
+
+        // by 「四則運算法」之「乘除法」
+        a = a * b;
+        b = a / b;
+        a = a / b;
+        // 一行寫法
+        a = a * b / (b = a);
+
+        // by 「XOR 運算法」
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        // 一行寫法
+        a = (a = a ^ b) ^ (b = a ^ b);
+
+        System.out.println("a: " + a + ", b: " + b);
+    }
+}
